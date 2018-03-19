ccdd65cf541d333b4dfff858603a58e3b6437520
Mylan Connolly <mylan.connolly@gmail.com>
2018-03-19 16:07:38 -0400

    Implemented some feature improvements

--- guessing_game.c
+++ guessing_game.c
@@ -6,13 +6,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 bool check_num(int rand_num, int guess_num) {
 	if (guess_num < rand_num) {
-		printf("Your guess is too low. Guess again!\n");
+		printf("Your guess is too low.\n");
 		return false;
 	} else if (guess_num > rand_num) {
-		printf("Your guess is too high. Guess again!\n");
+		printf("Your guess is too high.\n");
 		return false;
 	} else {
 		return true;
@@ -24,6 +25,15 @@ int gen_num(int max) {
 	return rand() % max + 1;
 }
 
+int parse_num(char value[]) {
+  int num = 0;
+
+  if (sscanf(value, "%d", &num) != 1) {
+    printf("That didn't work.... try using a valid number greater than 0.\n");
+  }
+  return num;
+}
+
 int ask_num(char prompt[]) {
 	int num = 0;
 	char input[100];
@@ -33,27 +43,51 @@ int ask_num(char prompt[]) {
 		fflush(stdout);
 
 		if (fgets(input, sizeof input, stdin) != NULL) {
-			if (sscanf(input, "%d", &num) != 1) {
-				num = 0;
-				printf("That didn't work...\n");
-			}
+      num = parse_num(input);
 		}
 	}
 	return num;
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	int rand_num = 0;
 	int guess_num = 0;
 	int counter = 0;
 	int max = 0;
+  int max_tries = 0;
+  int c;
 	bool correct = false;
 
-	max = ask_num("Enter a maximum number (must be a positive number):\n> ");
+  while ((c = getopt(argc, argv, "m:M:")) != -1) {
+    switch (c) {
+      case 'm':
+        max = parse_num(optarg);
+
+        if (max == 0) {
+          return 1;
+        }
+        break;
+      case 'M':
+        max_tries = parse_num(optarg);
+
+        if (max_tries == 0) {
+          return 1;
+        }
+        break;
+    }
+  }
+  if (max == 0) {
+    max = ask_num("Enter a maximum number (must be a positive number):\n> ");
+  }
 	rand_num = gen_num(max);
 
 	while (!correct) {
 		counter++;
+
+    if (max_tries > 0 && max_tries < counter) {
+      printf("You guessed wrong too many times. Try again!\n");
+      return 0;
+    }
 		guess_num = ask_num("Guess the number:\n> ");
 		correct = check_num(rand_num, guess_num);
 	}
